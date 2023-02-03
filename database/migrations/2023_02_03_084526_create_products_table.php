74c5fe64167bd30a36cd75b30bf0afb664619745
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2023-02-03 10:47:55 +0100

    Tablas creadas

--- database/migrations/2023_02_03_084526_create_products_table.php
+++ database/migrations/2023_02_03_084526_create_products_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->float("import");
+            $table->integer("stock");
+            $table->integer("code");
+            $table->text("description");
             $table->timestamps();
         });
     }
