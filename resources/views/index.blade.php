4d19fa9e5e5819b25cb17390332517d06fbec202
pmartinp <pmartinpdev@gmail.com>
2023-02-05 21:24:10 +0100

    carousel of landing

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,60 @@
 @extends('templates.general')
 
+@section('banner')
+<!--Carrusel de imágenes-->
+<div id="carouselId" class="carousel slide border-bottom" data-bs-ride="carousel">
+    <ol class="carousel-indicators">
+      <li
+        data-bs-target="#carouselId"
+        data-bs-slide-to="0"
+        class="active"
+        aria-current="true"
+        aria-label="First slide"
+      ></li>
+      <li
+        data-bs-target="#carouselId"
+        data-bs-slide-to="1"
+        aria-label="Second slide"
+      ></li>
+      <li
+        data-bs-target="#carouselId"
+        data-bs-slide-to="2"
+        aria-label="Third slide"
+      ></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <div class="carousel-item active">
+        <img src="./img/beready-logo.png" class="w-100 d-block" alt="imagen del carrusel" />
+      </div>
+      <div class="carousel-item">
+        <img src="./img/beready-logo.png" class="w-100 d-block" alt="imagen del carrusel" />
+      </div>
+      <div class="carousel-item">
+        <img
+          src="./img/beready-logo.png" class="w-100 d-block" alt="imagen del carrusel"/>
+      </div>
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselId"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselId"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+@endsection
+
 @section('body')
     <!-- Contenido -->
     <section id="products" class="section-products">
