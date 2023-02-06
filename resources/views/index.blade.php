1dde58531192296e37dbb34c0cefcc5f86d99342
jonygarcia <jonatangarciabarrera@gmail.com>
2023-02-06 14:09:28 +0100

    adjust images and discount

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,58 +1,34 @@
 @extends('templates.general')
 
 @section('banner')
-<!--Carrusel de imágenes-->
-<div id="carouselId" class="carousel slide border-bottom" data-bs-ride="carousel">
-    <ol class="carousel-indicators">
-      <li
-        data-bs-target="#carouselId"
-        data-bs-slide-to="0"
-        class="active"
-        aria-current="true"
-        aria-label="First slide"
-      ></li>
-      <li
-        data-bs-target="#carouselId"
-        data-bs-slide-to="1"
-        aria-label="Second slide"
-      ></li>
-      <li
-        data-bs-target="#carouselId"
-        data-bs-slide-to="2"
-        aria-label="Third slide"
-      ></li>
-    </ol>
-    <div class="carousel-inner" role="listbox">
-      <div class="carousel-item active">
-        <img src="./img/beready-logo.png" class="w-100 d-block" alt="imagen del carrusel" />
-      </div>
-      <div class="carousel-item">
-        <img src="./img/beready-logo.png" class="w-100 d-block" alt="imagen del carrusel" />
-      </div>
-      <div class="carousel-item">
-        <img
-          src="./img/beready-logo.png" class="w-100 d-block" alt="imagen del carrusel"/>
-      </div>
+    <!--Carrusel de imágenes-->
+    <div id="carouselId" class="carousel slide border-bottom" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide">
+            </li>
+            <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
+            <li data-bs-target="#carouselId" data-bs-slide-to="2" aria-label="Third slide"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <img src="./img/banner.webp" class="w-100 d-block" alt="imagen del carrusel" />
+            </div>
+            <div class="carousel-item">
+                <img src="./img/sanvalentin.webp" class="w-100 d-block" alt="imagen del carrusel" />
+            </div>
+            <div class="carousel-item">
+                <img src="./img/sanvalentin2.webp" class="w-100 d-block" alt="imagen del carrusel" />
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <button
-      class="carousel-control-prev"
-      type="button"
-      data-bs-target="#carouselId"
-      data-bs-slide="prev"
-    >
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button
-      class="carousel-control-next"
-      type="button"
-      data-bs-target="#carouselId"
-      data-bs-slide="next"
-    >
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
 @endsection
 
 @section('body')
@@ -73,9 +49,10 @@
                 <!-- Single Product -->
                 <div class="col-md-6 col-lg-4 col-xl-3">
                     <div id="product-1" class="single-product">
-                        <div class="part-1">
+                        <div class="part-1 text-center">
                             <span class="discount">40% descuento</span>
-                            <img class="w-75 m-0" src="./img/159-xiaomi-redmi-note-11s-6-128gb-gris-libre.webp" alt="imagen del producto">
+                            <img class="h-100 w-auto m-0" src="./img/159-xiaomi-redmi-note-11s-6-128gb-gris-libre.webp"
+                                alt="imagen del producto">
                             <ul>
                                 <li><i id="cart-1" class="bi bi-cart" onclick="checkIcon('cart-1')"></i></li>
                                 <li><i id="heart-1" class="bi bi-suit-heart" onclick="checkIcon('heart-1')"></i></li>
@@ -93,9 +70,10 @@
                 <!-- Single Product -->
                 <div class="col-md-6 col-lg-4 col-xl-3">
                     <div id="product-2" class="single-product">
-                        <div class="part-1">
+                        <div class="part-1 text-center">
                             <span class="discount">46% descuento</span>
-                            <img class="w-75 m-0" src="./img/126-apple-iphone-13-128gb-medianoche-libre.webp" alt="imagen del producto">
+                            <img class="h-100 w-auto m-0" src="./img/126-apple-iphone-13-128gb-medianoche-libre.webp"
+                                alt="imagen del producto">
                             <ul>
                                 <li><i id="cart-2" class="bi bi-cart" onclick="checkIcon('cart-2')"></i></li>
                                 <li><i id="heart-2" class="bi bi-suit-heart" onclick="checkIcon('heart-2')"></i></li>
@@ -113,9 +91,11 @@
                 <!-- Single Product -->
                 <div class="col-md-6 col-lg-4 col-xl-3">
                     <div id="product-3" class="single-product">
-                        <div class="part-1">
+                        <div class="part-1 text-center">
                             <span class="new">new</span>
-                            <img class="w-75 m-0" src="./img/1193-samsung-galaxy-s23-ultra-256gb-algodon-libre-cargador-25w.webp" alt="imagen del producto">
+                            <img class="h-100 w-auto m-0"
+                                src="./img/1193-samsung-galaxy-s23-ultra-256gb-algodon-libre-cargador-25w.webp"
+                                alt="imagen del producto">
                             <ul>
                                 <li><i id="cart-3" class="bi bi-cart" onclick="checkIcon('cart-3')"></i></li>
                                 <li><i id="heart-3" class="bi bi-suit-heart" onclick="checkIcon('heart-3')"></i></li>
@@ -133,9 +113,10 @@
                 <!-- Single Product -->
                 <div class="col-md-6 col-lg-4 col-xl-3">
                     <div id="product-4" class="single-product">
-                        <div class="part-1">
+                        <div class="part-1 text-center">
                             <span class="discount">33% descuento</span>
-                            <img class="w-75 m-0" src="./img/1752-samsung-galaxy-tab-s8-5g-128gb-gris-oscuro.webp" alt="imagen del producto">
+                            <img class="h-100 w-auto m-0" src="./img/1752-samsung-galaxy-tab-s8-5g-128gb-gris-oscuro.webp"
+                                alt="imagen del producto">
                             <ul>
                                 <li><i id="cart-4" class="bi bi-cart" onclick="checkIcon('cart-4')"></i></li>
                                 <li><i id="heart-4" class="bi bi-suit-heart" onclick="checkIcon('heart-4')"></i></li>
@@ -158,8 +139,10 @@
                 <!-- Single Product -->
                 <div class="col-md-6 col-lg-4 col-xl-3">
                     <div id="product-5" class="single-product">
-                        <div class="part-1">
-                            <img class="w-75 m-0" src="./img/1533-kioxia-exceria-g2-unidad-ssd-1tb-nvme-m2-2280.webp" alt="imagen del producto">
+                        <div class="part-1 text-center">
+                            <img class="h-100 w-auto m-0 text-center"
+                                src="./img/1533-kioxia-exceria-g2-unidad-ssd-1tb-nvme-m2-2280.webp"
+                                alt="imagen del producto">
                             <ul>
                                 <li><i id="cart-5" class="bi bi-cart" onclick="checkIcon('cart-5')"></i></li>
                                 <li><i id="heart-5" class="bi bi-suit-heart" onclick="checkIcon('heart-5')"></i></li>
@@ -177,9 +160,11 @@
                 <!-- Single Product -->
                 <div class="col-md-6 col-lg-4 col-xl-3">
                     <div id="product-6" class="single-product">
-                        <div class="part-1">
+                        <div class="part-1 text-center">
                             <span class="new">new</span>
-                            <img class="w-75 m-0" src="./img/1157-msi-geforce-rtx-3060-ventus-2x-oc-12gb-gddr6.webp" alt="imagen del producto">
+                            <img class="h-100 w-auto m-0"
+                                src="./img/1157-msi-geforce-rtx-3060-ventus-2x-oc-12gb-gddr6.webp"
+                                alt="imagen del producto">
                             <ul>
                                 <li><i id="cart-6" class="bi bi-cart" onclick="checkIcon('cart-6')"></i></li>
                                 <li><i id="heart-6" class="bi bi-suit-heart" onclick="checkIcon('heart-6')"></i></li>
@@ -197,9 +182,10 @@
                 <!-- Single Product -->
                 <div class="col-md-6 col-lg-4 col-xl-3">
                     <div id="product-7" class="single-product">
-                        <div class="part-1">
+                        <div class="part-1 text-center">
                             <span class="discount">30% descuento</span>
-                            <img class="w-75 m-0" src="./img/1636-intel-core-i5-12400f-44-ghz.webp" alt="imagen del producto">
+                            <img class="h-100 w-auto m-0" src="./img/1636-intel-core-i5-12400f-44-ghz.webp"
+                                alt="imagen del producto">
                             <ul>
                                 <li><i id="cart-7" class="bi bi-cart" onclick="checkIcon('cart-7')"></i></li>
                                 <li><i id="heart-7" class="bi bi-suit-heart" onclick="checkIcon('heart-7')"></i></li>
@@ -217,9 +203,11 @@
                 <!-- Single Product -->
                 <div class="col-md-6 col-lg-4 col-xl-3">
                     <div id="product-8" class="single-product">
-                        <div class="part-1">
+                        <div class="part-1 text-center">
                             <span class="discount">48% descuento</span>
-                            <img class="w-75 m-0" src="./img/1955-kingston-fury-beast-ddr5-5200mhz-32gb-2x16gb-cl40.webp" alt="imagen del producto">
+                            <img class="h-100 w-auto m-0"
+                                src="./img/1955-kingston-fury-beast-ddr5-5200mhz-32gb-2x16gb-cl40.webp"
+                                alt="imagen del producto">
                             <ul>
                                 <li><i id="cart-8" class="bi bi-cart" onclick="checkIcon('cart-8')"></i></li>
                                 <li><i id="heart-8" class="bi bi-suit-heart" onclick="checkIcon('heart-8')"></i></li>
