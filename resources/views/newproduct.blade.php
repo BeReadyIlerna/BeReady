06228f3969ce26d389cdd37cffaa7d0283ba5457
jonygarcia <jonatangarciabarrera@gmail.com>
2023-02-08 12:40:37 +0100

    form design

--- resources/views/newproduct.blade.php
+++ resources/views/newproduct.blade.php
@@ -2,18 +2,19 @@
 
 @section('body')
     <div class="div-body">
-        <div class="mt-5 mb-5 justify-content-center d-grid ">
+        <div class="mt-5 mb-5 justify-content-center d-grid container">
 
             <h2>Crear nuevo producto</h2>
 
-            <form action="{{ route('product.create') }}" method="POST" enctype='multipart/form-data'>
+            <form id="contactForm" action="{{ route('product.create') }}" method="POST" enctype='multipart/form-data'
+                data-sb-form-api-token="API_TOKEN">
                 @csrf {{-- Cláusula para obtener un token de formulario al enviarlo --}}
                 @if (session('message'))
                     <div class="alert alert-success">
                         {{ session('message') }}
                     </div>
                 @endif
-                
+
                 @error('name')
                     <div class="alert alert-danger">¡Ups! Hubo un error en el nombre</div>
                 @enderror
@@ -29,37 +30,60 @@
                 @error('description')
                     <div class="alert alert-danger">¡Ups! Hubo un error en la imagen</div>
                 @enderror
-                <label for="name">Nombre:</label>
-                <input type="text" name="name" value="{{ old('name') }}" placeholder="Nombre"
-                    class="form-control mb-2" autofocus>
+                <div class="form-floating mb-3">
+                    <input class="form-control" id="name" type="text" name="name" placeholder="Nombre"
+                        data-sb-validations="required" />
+                    <label for="nombre">Nombre</label>
+                    <div class="invalid-feedback" data-sb-feedback="name:required">El nombre no puede estar vacío</div>
+                </div>
+                <div class="form-floating mb-3">
+                    <input class="form-control" id="price" type="number" name="price" placeholder="Precio"
+                        data-sb-validations="required" />
+                    <label for="precio">Precio</label>
+                    <div class="invalid-feedback" data-sb-feedback="price:required">El precio no puede estar vacío</div>
+                </div>
+                <div class="form-floating mb-3">
+                    <input class="form-control" id="iva" type="number" name="IVA" placeholder="IVA"
+                        data-sb-validations="required" />
+                    <label for="iva">IVA</label>
+                    <div class="invalid-feedback" data-sb-feedback="iva:required">El IVA no puede estar vacío</div>
+                </div>
+                <div class="form-floating mb-3">
+                    <input class="form-control" id="stock" type="number" name="number" placeholder="Stock"
+                        data-sb-validations="required" />
+                    <label for="stock">Stock</label>
+                    <div class="invalid-feedback" data-sb-feedback="stock:required">El stock no puede estar vacío</div>
+                </div>
+                <div class="form-floating mb-3">
+                    
+                    
+                </div>
 
-                <label for="price">Precio:</label>
-                <input type="number" name="price" value="{{ old('price') }}" placeholder="Precio"
-                    class="form-control mb-2">
-
-                <label for="IVA">IVA:</label>
-                <input type="number" name="IVA" value="{{ old('IVA') }}" placeholder="IVA"
-                    class="form-control mb-2">
-
-                <label for="stock">Stock:</label>
-                <input type="number" name="stock" value="{{ old('stock') }}" placeholder="Stock"
-                    class="form-control mb-2">
-
-                <label for="description">Descripción:</label>
-                <textarea type="text" name="description" value="{{ old('description') }}" placeholder="Descripción"
-                    class="form-control mb-2"></textarea>
-
+                <div class="form-floating mb-3">
+                    <textarea class="form-control" id="description" type="text" name="description" placeholder="Descripción"
+                        style="height: 10rem;" data-sb-validations="required"></textarea>
+                    <label for="descripcion">Descripción</label>
+                    <div class="invalid-feedback" data-sb-feedback="description:required">La descripción no puede estar
+                        vacía</div>
+                </div>
                 <label for="image">Imagen:</label>
                 <input type="file" name="image" accept="image/png, image/jpeg, image/webp">
-                <br>
-                <br>
-                <button class="btn btn-primary btn-block text-center" type="submit">
-                    Introducir nuevo producto
-                </button>
-
-
-
+                <div class="d-none" id="submitSuccessMessage">
+                    <div class="text-center mb-3">
+                        <div class="fw-bolder">Form submission successful!</div>
+                        <p>To activate this form, sign up at</p>
+                        <a
+                            href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                    </div>
+                </div>
+                <div class="d-none" id="submitErrorMessage">
+                    <div class="text-center text-danger mb-3">Error sending message!</div>
+                </div>
+                <div class="d-grid">
+                    <button class="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button>
+                </div>
             </form>
+
         </div>
     </div>
 @endsection
--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -8,13 +8,14 @@
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="./css/custom.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    
+
     <script defer src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
         integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
     </script>
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
         integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script>
+    <script defer src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
     <title>@yield('title')</title>
 </head>
 
