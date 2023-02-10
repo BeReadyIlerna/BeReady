41845a29e6ae5cc7bf28c70f45dff2255c0d9027
Lfparr86 <115531684+Lfparr86@users.noreply.github.com>
2023-02-10 09:59:39 +0100

    Update signup.blade.php

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,33 +1,6 @@
 @extends('templates.general')
 @section('body')
 
-{{-- <div class="container px-5 my-5">
-    <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-        <div class="form-floating mb-3">
-            <input class="form-control" id="newField" type="text" placeholder="New Field" data-sb-validations="required" />
-            <label for="newField">New Field</label>
-            <div class="invalid-feedback" data-sb-feedback="newField:required">New Field is required.</div>
-        </div>
-        <div class="form-floating mb-3">
-            <textarea class="form-control" id="message" type="text" placeholder="Message" style="height: 10rem;" data-sb-validations="required"></textarea>
-            <label for="message">Message</label>
-            <div class="invalid-feedback" data-sb-feedback="message:required">Message is required.</div>
-        </div>
-        <div class="d-none" id="submitSuccessMessage">
-            <div class="text-center mb-3">
-                <div class="fw-bolder">Form submission successful!</div>
-                <p>To activate this form, sign up at</p>
-                <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-            </div>
-        </div>
-        <div class="d-none" id="submitErrorMessage">
-            <div class="text-center text-danger mb-3">Error sending message!</div>
-        </div>
-        <div class="d-grid">
-            <button class="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button>
-        </div>
-    </form>
-</div> --}}
 
 
 @if (session('message'))
