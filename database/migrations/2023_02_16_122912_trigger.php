4f76f9ba848e03ecd32d8827968d7b1c31027f15
pmartinp <pmartinpdev@gmail.com>
2023-02-16 13:52:02 +0100

    Trigger for shoppingcart completed

--- database/migrations/2023_02_16_122912_trigger.php
+++ database/migrations/2023_02_16_122912_trigger.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class Trigger extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +14,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('shoppingcarts', function (Blueprint $table) {
-            $table->id();
-            $table->float("total")->default(0);
-            $table->foreignId("user_id")->references("id")->on("users");
-            $table->timestamps();
-        });
+        DB::unprepared('CREATE TRIGGER add_cart AFTER INSERT ON `USERS` FOR EACH ROW
+        BEGIN
+           INSERT INTO `shoppingcarts` SET user_id = NEW.id, created_at = NEW.created_at, updated_at = NEW.updated_at;
+        END');
     }
 
     /**
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('shoppingcarts');
+        DB::unprepared('DROP TRIGGER `add_cart`');
     }
 };
