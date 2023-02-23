c32b0aff5d384f9e508d74a2cd3291d6409b2611
pmartinp <pmartinpdev@gmail.com>
2023-02-23 19:57:27 +0100

    product stock controlled

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,12 +1,17 @@
 @extends('templates.general')
 
 @section('title')
-    BeReady - {{$product->name}}
+    BeReady - {{ $product->name }}
 @endsection
 
 @section('body')
     <section class="container d-grid py-5 mb-5">
         <div class="row">
+            @if (session('error'))
+                <div class="alert alert-danger">
+                    {{ session('error') }}
+                </div>
+            @endif
             <!--IMAGE-->
             <div class="col-lg-6 d-flex justify-content-end">
                 <img class="product-image mt-4" src="{{ URL::asset('img/' . $product->image) }}" alt="imagen del producto">
@@ -34,36 +39,39 @@
                     <form action="{{ route('cart.addProduct') }}" method="POST">
                         @csrf
                         <!--QUANTITY-->
-                    <div class="row mt-4">
-                        <span class="fw-bold col-4 align-self-center">Cantidad: </span>
-                        <span class="fw-normal col-8">
-                            <div class="d-flex mb-md-0">
-                                <button class="btn px-2" type="button"
-                                    onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                    <i class="bi bi-dash-lg"></i>
-                                </button>
+                        <div class="row mt-4">
+                            <span class="fw-bold col-4 align-self-center">Cantidad: </span>
+                            <span class="fw-normal col-8">
+                                <div class="d-flex mb-md-0">
+                                    <button class="btn px-2" type="button"
+                                        onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                        <i class="bi bi-dash-lg"></i>
+                                    </button>
 
-                                <input id="form1" min="1" max="99" name="quantity" value="1" type="number"
-                                    class="form-control form-control-sm product-quantity-width text-center" />
+                                    <input id="form1" min="1" max="99" name="quantity" value="1"
+                                        type="number"
+                                        class="form-control form-control-sm product-quantity-width text-center" />
 
-                                <button class="btn px-2" type="button"
-                                    onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                    <i class="bi bi-plus-lg"></i>
-                                </button>
-                            </div>
-                        </span>
-                    </div>
-                    <input id="form1" name="product" value="{{$product->id}}" hidden />
-                    <!--ADD SHOPPING CART-->
-                    <div class="row mt-4">
-                        <button class="btn btn-primary p-3 col-5" type="submit"><h5 class="m-0">Añadir al carrito</h5></button>
-                    </div>
+                                    <button class="btn px-2" type="button"
+                                        onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                        <i class="bi bi-plus-lg"></i>
+                                    </button>
+                                </div>
+                            </span>
+                        </div>
+                        <input id="form1" name="product" value="{{ $product->id }}" hidden />
+                        <!--ADD SHOPPING CART-->
+                        <div class="row mt-4">
+                            <button class="btn btn-primary p-3 col-5" type="submit">
+                                <h5 class="m-0">Añadir al carrito</h5>
+                            </button>
+                        </div>
                     </form>
-                    
+
                     <div class="row mt-4">
                         <span class="fw-bold col-4">Envío: </span>
                         <span class="fw-bold text-success col-8">Envío GRATIS <a href="#"><i
-                            class="bi bi-info-circle-fill text-dark"></i></a></span>
+                                    class="bi bi-info-circle-fill text-dark"></i></a></span>
                     </div>
                     <div class="row mt-4">
                         <span class="fw-bold col-4">Devolución: </span>
